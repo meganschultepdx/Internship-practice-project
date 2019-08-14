6500590c16213a7a5458db718da60a0a554ea2c3
megan schulte <megan.schulte.pdx@gmail.com>
2019-08-14 10:25:25 -0700

    update code in the program.cs file

--- Program.cs
+++ Program.cs
@@ -2,11 +2,23 @@
 
 namespace ConsoleApp.SQLite
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            using (var db = new BloggingContext())
+            {
+                db.Blogs.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+                var count = db.SaveChanges();
+                Console.WriteLine("{0} records saved to database", count);
+
+                Console.WriteLine();
+                Console.WriteLine("All blogs in database:");
+                foreach (var blog in db.Blogs)
+                {
+                    Console.WriteLine(" - {0}", blog.Url);
+                }
+            }
         }
     }
 }
